86bf3b323c68807c128a233db1a5e8922e468d04
unknown <bossrc64@outlook.com>
2024-03-08 12:06:12 +0530

    Added Item in the screen

--- app/src/main/java/com/example/humaranews/MainActivity.kt
+++ app/src/main/java/com/example/humaranews/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.humaranews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.humaranews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerViewList.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        val items = fetchData()
+        val adapter: NewsListAdapter = NewsListAdapter(items)
+        binding.recyclerViewList.adapter = adapter
+    }
+    private fun fetchData(): ArrayList<String> {
+        val list = ArrayList<String>()
+        for(i in 0 until 100) {
+            list.add("Item $i")
+        }
+        return list
     }
 }
