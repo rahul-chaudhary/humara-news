cdd4491c063d80f60ca296beb48e4fe2d3f76822
unknown <bossrc64@outlook.com>
2024-03-08 14:42:43 +0530

    Added glide dependency in the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
     implementation("androidx.databinding:databinding-runtime:$version")
     //Volley
     implementation ("com.android.volley:volley:1.2.1")
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
