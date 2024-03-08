24eecb006f32c1ca9197fb1dd261965f5d6b6c0f
unknown <bossrc64@outlook.com>
2024-03-08 13:08:41 +0530

    Added MySingleton class in the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //data binding
     val version = "8.3.0"
     implementation("androidx.databinding:databinding-runtime:$version")
+    //Volley
+    implementation ("com.android.volley:volley:1.2.1")
 }
