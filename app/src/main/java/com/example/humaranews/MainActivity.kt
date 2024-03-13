0f9d7063fed774d6b4f8402360aca74b6fee026c
unknown <bossrc64@outlook.com>
2024-03-13 12:49:00 +0530

    Added Slash screen in MainActivity

--- app/src/main/java/com/example/humaranews/MainActivity.kt
+++ app/src/main/java/com/example/humaranews/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.humaranews
 
 import android.R
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.example.humaranews.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +27,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        lifecycleScope.launch {
+            delay(1000)
+            val intent = Intent(this@MainActivity, HomeScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
 
+//        Without using coroutines
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this, HomeScreen::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 1000)
     }
 }
